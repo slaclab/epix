342b8f3d84c25e592c0513b6a4fb85c49ba529c9
Dan Sciortino <disco@slac.stanford.edu>
2022-06-24 12:03:22 -0700

    updating rogue to match new aes-stream-driver version

--- software/Epix/setup_template.sh
+++ software/Epix/setup_template.sh
@@ -1,9 +1,20 @@
 # Setup environment
-source /afs/slac/g/reseng/rogue/v3.7.0/setup_rogue.sh
+#source /afs/slac/g/reseng/rogue/v3.7.0/setup_rogue.sh
+#
+## Python Package directories
+#export EPIXROGUE_DIR=${PWD}/python
+#export SURF_DIR=${PWD}/../../firmware/submodules/surf/python
+#
+## Setup python path
+#export PYTHONPATH=${PWD}/python:${EPIXROGUE_DIR}:${SURF_DIR}:${PYTHONPATH}
 
-# Python Package directories
-export EPIXROGUE_DIR=${PWD}/python
-export SURF_DIR=${PWD}/../../firmware/submodules/surf/python
 
-# Setup python path
-export PYTHONPATH=${PWD}/python:${EPIXROGUE_DIR}:${SURF_DIR}:${PYTHONPATH}
+##################################
+# Setup environment
+##################################
+source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
+
+##################################
+# Activate Rogue conda Environment
+##################################
+conda activate rogue_v5.14.0
